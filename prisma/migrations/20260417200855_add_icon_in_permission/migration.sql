166218441413c470a6d879a1f86f25f079ed583d
Ashir Arif <techashir167@gmail.com>
2026-04-21 11:01:17 +0500

    fix: restructure DB schemas

--- prisma/migrations/20260417200855_add_icon_in_permission/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `permissions` ADD COLUMN `icon` VARCHAR(191) NULL DEFAULT 'House';
--- prisma/migrations/20260421054348_init/migration.sql
+++ prisma/migrations/20260421054348_init/migration.sql
@@ -10,10 +10,9 @@ CREATE TABLE `roles` (
     `createdBy` INTEGER NULL,
     `updatedBy` INTEGER NULL,
     `deletedBy` INTEGER NULL,
-    `tenantId` INTEGER NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
 
     UNIQUE INDEX `roles_slug_key`(`slug`),
-    UNIQUE INDEX `roles_tenantId_slug_key`(`tenantId`, `slug`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -23,6 +22,7 @@ CREATE TABLE `permissions` (
     `name` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NULL,
+    `icon` VARCHAR(191) NULL DEFAULT 'House',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `deletedAt` DATETIME(3) NULL,
@@ -30,7 +30,6 @@ CREATE TABLE `permissions` (
     `updatedBy` INTEGER NULL,
     `deletedBy` INTEGER NULL,
     `isActive` BOOLEAN NOT NULL DEFAULT true,
-    `isApproved` BOOLEAN NOT NULL DEFAULT false,
     `roleId` INTEGER NOT NULL,
 
     UNIQUE INDEX `permissions_roleId_slug_key`(`roleId`, `slug`),
@@ -38,31 +37,38 @@ CREATE TABLE `permissions` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `tenants` (
+CREATE TABLE `role_permissions` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `roleId` INTEGER NOT NULL,
+    `permissionId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `role_permissions_roleId_permissionId_key`(`roleId`, `permissionId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `agents` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fullName` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `slug` VARCHAR(191) NOT NULL,
-    `phone` VARCHAR(191) NULL,
-    `status` ENUM('ACTIVE', 'INACTIVE', 'SUSPENDED') NOT NULL DEFAULT 'ACTIVE',
-    `logo` VARCHAR(191) NULL,
-    `address` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `deletedAt` DATETIME(3) NULL,
     `createdBy` INTEGER NULL,
     `updatedBy` INTEGER NULL,
     `deletedBy` INTEGER NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `roleId` INTEGER NOT NULL,
+    `psaId` INTEGER NULL,
 
-    UNIQUE INDEX `tenants_email_key`(`email`),
-    UNIQUE INDEX `tenants_slug_key`(`slug`),
+    UNIQUE INDEX `agents_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NULL,
@@ -72,21 +78,31 @@ CREATE TABLE `users` (
     `createdBy` INTEGER NULL,
     `updatedBy` INTEGER NULL,
     `deletedBy` INTEGER NULL,
-    `tenantId` INTEGER NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
     `roleId` INTEGER NOT NULL,
+    `agentId` INTEGER NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `roles` ADD CONSTRAINT `roles_tenantId_fkey` FOREIGN KEY (`tenantId`) REFERENCES `tenants`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE `permissions` ADD CONSTRAINT `permissions_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `roles`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `users` ADD CONSTRAINT `users_tenantId_fkey` FOREIGN KEY (`tenantId`) REFERENCES `tenants`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `role_permissions` ADD CONSTRAINT `role_permissions_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `roles`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `role_permissions` ADD CONSTRAINT `role_permissions_permissionId_fkey` FOREIGN KEY (`permissionId`) REFERENCES `permissions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `agents` ADD CONSTRAINT `agents_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `roles`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `agents` ADD CONSTRAINT `agents_psaId_fkey` FOREIGN KEY (`psaId`) REFERENCES `agents`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `users` ADD CONSTRAINT `users_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `roles`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_agentId_fkey` FOREIGN KEY (`agentId`) REFERENCES `agents`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
